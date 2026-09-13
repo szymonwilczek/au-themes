8fc541b10a42f6387443bff6f41c9aa1bad94444
Szymon Wilczek <swilczek.lx@gmail.com>
2026-09-13 22:24:46 +0200

    docs: add canonical GNU ELPA/MELPA GPLv3 headers and commentary across all themes
    Add standard Conventional Headers for Emacs Libraries (GNU ELPA / MELPA format):
    - Full GNU General Public License v3 header.
    - Explicit Copyright.
    - Author, Maintainer, URL, Keywords metadata.
    - Structured ;;; Commentary: and ;;; Code: sections.
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- au-aurum-day-theme.el
+++ au-aurum-day-theme.el
@@ -1,7 +1,34 @@
 ;;; au-aurum-day-theme.el --- Warm sandstone, ancient gold, and basalt daytime theme -*- lexical-binding:t -*-
 
+;; Copyright (C) 2026  Szymon Wilczek
+
 ;; Author: Szymon Wilczek <swilczek.lx@gmail.com>
+;; Maintainer: Szymon Wilczek <swilczek.lx@gmail.com>
+;; URL: https://github.com/szymonwilczek/au-themes
 ;; Keywords: faces, themes, accessibility, autism, neurodivergence
+
+;; This file is NOT part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Warm sandstone, ancient gold, and basalt daylight sanctuary with zero blue
+;; light hazard. Part of the `au-themes' collection engineered for sensory-safe
+;; autistic and neurodivergent visual perception.
+
+;;; Code:
 
 (require 'ef-themes)
 
--- au-themes.el
+++ au-themes.el
@@ -1,10 +1,28 @@
 ;;; au-themes.el --- Sensory-safe, scientifically gated themes for autism and neurodivergence -*- lexical-binding: t -*-
 
+;; Copyright (C) 2026  Szymon Wilczek
+
 ;; Author: Szymon Wilczek <swilczek.lx@gmail.com>
+;; Maintainer: Szymon Wilczek <swilczek.lx@gmail.com>
+;; URL: https://github.com/szymonwilczek/au-themes
 ;; Version: 0.2.0
 ;; Package-Requires: ((emacs "28.1") (ef-themes "1.0.0"))
 ;; Keywords: faces, themes, accessibility, autism, neurodivergence
-;; URL: https://github.com/szymonwilczek/au-themes
+
+;; This file is NOT part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
